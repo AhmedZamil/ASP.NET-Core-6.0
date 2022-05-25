a5b4bfe2209b13d7b282afa1d1ffb4f1eb950ac4
AhmedZamil <ahmedlutfulzamil@gmail.com>
2022-05-25 23:07:30 +0600

    Clean solution

--- Marielinas/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Marielinas.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
